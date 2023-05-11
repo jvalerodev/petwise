6a82b29af88defcf0aa05a1df07c6d50485de5ad
Jesús Valero <jvalerodev@gmail.com>
2023-05-11 00:48:57 -0400

    add more routes to matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -20,5 +20,5 @@ export const middleware = async (req: NextRequest) => {
 };
 
 export const config = {
-  matcher: ['/']
+  matcher: ['/', '/owners', '/pets', '/appointments', '/reports', '/settings']
 };
